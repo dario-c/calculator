8e1715203fa0d9f974e71f6f6e04d44f2ae3f59d
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-08-14 16:45:00 +0200

    Added taxes functionality for diff countries

--- calculator/app/controllers/calculato_controller.rb
+++ calculator/app/controllers/calculato_controller.rb
@@ -1,13 +1,26 @@
 class CalculatoController < ApplicationController
 
-def new
-end
 
-def create
-	@quantity = params[:quantity].to_i
-	@price = (params[:price]).to_i
-	@subtotal = @quantity*@price
-	render 'show'
-end
+	def new
+	end
+
+TAX_TABLE = {
+			es: 1.21,
+			de: 1.19,
+			fr: 1.12,
+			no: 1.35,
+			it: 1.20,
+			ph: 1.12
+		}
+
+	def create
+		@quantity = params[:quantity].to_i
+		@price = (params[:price]).to_i
+		@country_code = params[:country_code]
+		@tax_rate = TAX_TABLE[@country_code.to_sym]
+		@subtotal = (@quantity*@price)*(@tax_rate)
+		@subtotal.round(2)
+		render 'show'
+	end
 
 end
